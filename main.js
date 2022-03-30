86b9fbe8380377ba572d6dff50d1ebcf0bd600a2
Yossapon00 <kewsopap@gmail.com>
2022-03-30 10:46:21 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            inStock: true,
+            inventory: 0,
+            url:'https://www.vuemastery.com/courses-path/beginner'
         }
     }
 })
