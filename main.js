0134e36d6c4b2bbb24b4dcae000d1b9512b88433
Yossapon00 <kewsopap@gmail.com>
2022-04-20 10:15:50 +0700

    update

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
+            selectedVariant:0,
             image: './assets/images/socks_blue.jpg',
             inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ]
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].image
         }
     },
     methods: {
@@ -18,7 +30,8 @@ const app = Vue.createApp({
             this.cart += 1
         },
         updateImage(variantImage) {
-            this.image = variantImage
+            this.selectedVariant = index
+            console.log
         }
     }
 })
